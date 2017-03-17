eaa15f52dc0a72f4e13f2dd98afb75b7cd8318c6
Janne <suom.jann@gmail.com>
2017-03-17 20:52:27 +0200

    Sivua ei löydy

--- src/main/java/sparkscheduler/Sparkscheduler.java
+++ src/main/java/sparkscheduler/Sparkscheduler.java
@@ -1,17 +1,23 @@
 package sparkscheduler;
 
-import java.net.URI;
 import static spark.Spark.port;
 import static spark.Spark.get;
+import static spark.Spark.notFound;
+import static spark.Spark.staticFiles;
 
 public class Sparkscheduler {
     public static void main(String[] args) {
         getConnection();
-        
+        staticFiles.location("/static");
+
         // Once Heroku Postgres has been added a DATABASE_URL setting will be available in the app
         // configuration and will contain the URL used to access the Heroku Postgres service
-        get("/", (req, res) -> "Hello sparkscheduler!! \n"
-                + "The host of the database is " + new URI(System.getenv("DATABASE_URL")).getHost());
+        get("/", (req, res) -> "Hello sparkscheduler!!");
+
+        notFound((req, res) -> {
+            res.redirect("404.html");
+            return "";
+        });
     }
 
     private static void getConnection() {
