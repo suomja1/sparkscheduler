8d221011639bb6fb08c65e92cda89051dccc814f
Janne <suom.jann@gmail.com>
2017-03-23 10:18:36 +0200

    findAll-metodi

--- src/main/java/sparkscheduler/dao/EmployeeDao.java
+++ src/main/java/sparkscheduler/dao/EmployeeDao.java
@@ -1,6 +1,9 @@
 package sparkscheduler.dao;
 
+import java.util.List;
+import org.sql2o.Connection;
 import org.sql2o.Sql2o;
+import sparkscheduler.model.Employee;
 
 public class EmployeeDao {
     private Sql2o sql2o;
@@ -9,5 +12,10 @@ public class EmployeeDao {
         this.sql2o = sql2o;
     }
     
-    
+    public List<Employee> findAllByOrderByLastName() {
+        try (Connection c = sql2o.open()) {
+            return c.createQuery("SELECT * FROM Employee ORDER BY SUBSTRING(fullName, E'([^\\s]+)(,|$)')")
+                    .executeAndFetch(Employee.class);
+        }
+    }
 }
