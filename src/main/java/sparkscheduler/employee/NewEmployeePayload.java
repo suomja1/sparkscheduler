f73a4c82efb76f7b994dd020566c884891996ac4
Janne <suom.jann@gmail.com>
2017-03-29 20:38:14 +0300

    Uusi yritys

--- src/main/java/sparkscheduler/employee/NewEmployeePayload.java
+++ src/main/java/sparkscheduler/employee/NewEmployeePayload.java
@@ -7,16 +7,15 @@ import static sparkscheduler.Application.employeeDao;
 
 @Data
 public class NewEmployeePayload implements Validable {
-    private UUID superior;
     private String fullName;
+    private UUID superior;
+    private Double contract;
     private String username;
     private String password;
-    private Double contract;
 
     @Override
     public boolean isValid() {
         return fullName != null && !fullName.isEmpty()
-                && fullName != null && !fullName.isEmpty()
                 && username != null && !username.isEmpty() && !employeeDao.existsByUsername(username)
                 && password != null && !password.isEmpty();
     }
